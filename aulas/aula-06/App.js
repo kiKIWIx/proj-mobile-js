f0beb3fa0d1c460cb20b4e3c931b5ccbf992334f
Oliveira <biel07097@gmail.com>
2022-12-08 17:04:36 -0300

    Corrigindo aula 06

--- aulas/aula-06/App.js
+++ aulas/aula-06/App.js
@@ -3,11 +3,10 @@ import { SafeAreaView,  StyleSheet } from 'react-native'
 import Primeiro, { Segundo, Terceiro} from './components/Mult'
 import Estilo from './components/Estilo'
 
-export default()=>{
+export default()=>(
   <SafeAreaView style={Estilo.App}>
     <Primeiro/>
     <Segundo/>
     <Terceiro/>
-  </SafeAreaView>      
-}
-
+  </SafeAreaView>  
+)
--- aulas/aula-06/components/Mult.js
+++ aulas/aula-06/components/Mult.js
@@ -3,19 +3,13 @@ import { Text } from 'react-native'
 import Estilo from './Estilo'
 
 function Primeiro(){
-  return(
-    <Text style={Estilo.txt1, Estilo.box}>Primeiro</Text>
-  )
+  return <Text style={[Estilo.txt1, Estilo.box].join(" ")}>Primeiro</Text>
 }
 function Segundo(){
-  return(
-    <Text style={Estilo.txt2, Estilo.box}>Segundo</Text>
-  )
+  return <Text style={[Estilo.txt2, Estilo.box].join(" ")}>Segundo</Text>
 }
 function Terceiro(){
-  return(
-    <Text style={Estilo.txt3, Estilo.box}>Terceiro</Text>
-  )
+  return <Text style={[Estilo.txt3, Estilo.box].join(" ")}>Terceiro</Text>
 }
 
 export default Primeiro
