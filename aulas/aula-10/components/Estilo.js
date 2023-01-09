f8b6459334b68cbbf95255293d54184493d5e370
Oliveira <biel07097@gmail.com>
2023-01-09 15:46:01 -0300

    Botão e janela de alerta

---
+++ aulas/aula-10/components/Estilo.js
@@ -0,0 +1,26 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  txt6:{
+    fontSize: 18,
+    backgroundColor: 'gray',
+    color: 'white',
+    borderRadius: 10,
+    paddingBotton: 2.5,
+    paddingTop: 2.5,
+    paddingLeft: 10,
+    paddingRight: 10,
+    textAlign: "center",
+    margin: 2
+  },
+  txtS:{
+    fontSize: 12,
+    color: 'gray',
+    textAlign: "center",
+  },
+  obj:{flexDirection: 'row',
+  flex: 1,
+  justifyContent: 'center',
+  marginVertical: 8
+  }
+})
